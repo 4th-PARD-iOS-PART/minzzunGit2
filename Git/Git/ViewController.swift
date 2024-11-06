1c6392088abc8e604ffee0b1886012b04b5d4b54
Seunghun-Hyeon <tmdgns4970@handong.ac.kr>
2024-11-06 21:30:54 +0900

    [Fix] : branch_C

--- Git/Git/ViewController.swift
+++ Git/Git/ViewController.swift
@@ -2,11 +2,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // mainLabel 부분만 수정하세요!
     private let mainLabel: UILabel = {
         let label = UILabel()
-        label.text = "안녕하세요"
+        label.text = "C의 라벨입니다"  // 이 부분을 수정
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 24)
+        label.font = .italicSystemFont(ofSize: 35)  // 이 부분을 수정
+        label.textColor = .green  // 이 부분을 추가
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
