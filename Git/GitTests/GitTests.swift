aa338a5adc754c05fdacee9831494ccb904fdaf5
minjun kim <minzzzun@gmail.com>
2024-11-06 21:19:09 +0900

    생성

---
+++ Git/GitTests/GitTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTests.swift
+//  GitTests
+//
+//  Created by 김민준 on 11/6/24.
+//
+
+import Testing
+@testable import Git
+
+struct GitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
