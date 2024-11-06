4c10fa9273d5b9d0a8cf3d30c3d3e2e17899a1a1
minjun kim <minzzzun@gmail.com>
2024-11-06 22:01:25 +0900

    View 추가

---
+++ Git/Git/View.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+class View : UIView {
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+}
